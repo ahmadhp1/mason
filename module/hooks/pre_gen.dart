2bc51b603c90e8b1d86c96151d823ba3be04e80d
Ahmad <heydarpanah92@gmail.com>
2022-10-14 13:16:51 +0330

    refactor vars

--- module/hooks/pre_gen.dart
+++ module/hooks/pre_gen.dart
@@ -6,42 +6,42 @@ void run(HookContext context) {
   // TODO : get states and events parameters from user
 
   final moduleName = context.vars['moduleName'];
-
-  //*Class names *****************
   final moduleNamePascalCase = Casing.pascalCase(moduleName);
-
-  context.vars['Model'] = '${moduleNamePascalCase}Model';
-  context.vars['Dto'] = '${moduleNamePascalCase}Dto';
-  context.vars['Bloc'] = '${moduleNamePascalCase}Bloc';
-  context.vars['State'] = '${moduleNamePascalCase}State';
-  context.vars['Event'] = '${moduleNamePascalCase}Event';
-  context.vars['Failure'] = '${moduleNamePascalCase}Failure';
-
-  context.vars['Repository'] = 'I${moduleNamePascalCase}Repository';
-  context.vars['DataSource'] = 'I${moduleNamePascalCase}ApiDataSource';
-  context.vars['ApiService'] = 'I${moduleNamePascalCase}ApiService';
-
-  context.vars['RepositoryImp'] =
-      '${moduleNamePascalCase}RepositoryImplementation';
-  context.vars['DataSourceImp'] =
-      '${moduleNamePascalCase}ApiDataSourceImplementation';
-  context.vars['ApiServiceImp'] =
-      '${moduleNamePascalCase}ApiServiceImplementation';
-
-  //*File names *****************
   final moduleNameSnakeCase = Casing.snakeCase(moduleName);
 
-  context.vars['model_fn'] = '${moduleNameSnakeCase}_model';
-  context.vars['model_exports_fn'] = '${moduleNameSnakeCase}_models';
-  context.vars['dto_fn'] = '${moduleNameSnakeCase}_dto';
-  context.vars['failure_fn'] = '${moduleNameSnakeCase}_failure';
-  context.vars['bloc_fn'] = '${moduleNameSnakeCase}_bloc';
-  context.vars['state_fn'] = '${moduleNameSnakeCase}_state';
-  context.vars['event_fn'] = '${moduleNameSnakeCase}_event';
+  context.vars = {
+    ...context.vars,
 
-  context.vars['repository_fn'] = '${moduleNameSnakeCase}_repository';
-  context.vars['data_source_fn'] = '${moduleNameSnakeCase}_data_source';
-  context.vars['api_service_fn'] = '${moduleNameSnakeCase}_api_services';
-  context.vars['repository_imp_fn'] =
-      '${moduleNameSnakeCase}_repository_implementation';
+    //*Class names *****************
+
+    'Model': '${moduleNamePascalCase}Model',
+    'Dto': '${moduleNamePascalCase}Dto',
+    'Bloc': '${moduleNamePascalCase}Bloc',
+    'State': '${moduleNamePascalCase}State',
+    'Event': '${moduleNamePascalCase}Event',
+    'Failure': '${moduleNamePascalCase}Failure',
+
+    'Repository': 'I${moduleNamePascalCase}Repository',
+    'DataSource': 'I${moduleNamePascalCase}ApiDataSource',
+    'ApiService': 'I${moduleNamePascalCase}ApiService',
+
+    'RepositoryImp': '${moduleNamePascalCase}RepositoryImplementation',
+    'DataSourceImp': '${moduleNamePascalCase}ApiDataSourceImplementation',
+    'ApiServiceImp': '${moduleNamePascalCase}ApiServiceImplementation',
+
+    //*File names *****************
+
+    'model_fn': '${moduleNameSnakeCase}_model',
+    'model_exports_fn': '${moduleNameSnakeCase}_models',
+    'dto_fn': '${moduleNameSnakeCase}_dto',
+    'failure_fn': '${moduleNameSnakeCase}_failure',
+    'bloc_fn': '${moduleNameSnakeCase}_bloc',
+    'state_fn': '${moduleNameSnakeCase}_state',
+    'event_fn': '${moduleNameSnakeCase}_event',
+
+    'repository_fn': '${moduleNameSnakeCase}_repository',
+    'data_source_fn': '${moduleNameSnakeCase}_data_source',
+    'api_service_fn': '${moduleNameSnakeCase}_api_services',
+    'repository_imp_fn': '${moduleNameSnakeCase}_repository_implementation',
+  };
 }
